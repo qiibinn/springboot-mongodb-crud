2cd3491711aa1a94e3a922e8427e8dd95f15da61
qiibinn <17600104008@139.com>
2023-01-18 13:24:43 +0800

    Update Sighting.java

--- src/main/java/com/example/demo/model/Sighting.java
+++ src/main/java/com/example/demo/model/Sighting.java
@@ -7,16 +7,23 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "Sighting")
 public class Sighting {
+    private String id;
     private String birdName;
     private String location;
     private String datetime;
 
-    public Sighting(@JsonProperty("birdName") String birdName,
+    public Sighting(@JsonProperty("id") String id,
+                    @JsonProperty("birdName") String birdName,
                     @JsonProperty("location") String location,
                     @JsonProperty("datetime") String datetime) {
+        this.id = id;
         this.birdName = birdName;
         this.location = location;
         this.datetime = datetime;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getBirdName() {
@@ -31,6 +38,10 @@ public class Sighting {
         return datetime;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+    
     public void setBirdName(String birdName) {
         this.birdName = birdName;
     }
