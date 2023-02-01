76c246379417e6fc6a51217a41af0aa8ab2a631b
qiibinn <17600104008@139.com>
2023-02-01 10:57:12 +0800

    Update SightingRepository.java

--- src/main/java/com/example/demo/dao/SightingRepository.java
+++ src/main/java/com/example/demo/dao/SightingRepository.java
@@ -1,10 +1,17 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Sighting;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SightingRepository extends MongoRepository<Sighting, String> {
+
+    List<Sighting> findByBirdName(String birdName);
+    List<Sighting> findByLocation(String location);
+    List<Sighting> findByBirdNameAndLocation(String birdName, String location);
 
 }
