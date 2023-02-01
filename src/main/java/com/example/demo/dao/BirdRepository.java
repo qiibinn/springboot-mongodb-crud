435b46abfcb9c35c5ec39ae0273b7954c33034c4
qiibinn <17600104008@139.com>
2023-02-01 10:56:16 +0800

    Update BirdRepository.java

--- src/main/java/com/example/demo/dao/BirdRepository.java
+++ src/main/java/com/example/demo/dao/BirdRepository.java
@@ -1,10 +1,16 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Bird;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BirdRepository extends MongoRepository<Bird, String> {
-
+    List<Bird> findByColor(String color);
+    Optional<Bird> findByName(String name);
+    List<Bird> findByNameAndColor(String name, String color);
 }
