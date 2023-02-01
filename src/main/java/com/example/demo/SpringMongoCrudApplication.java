5dfb96df76dad1aeea465256e5dfb6b57788c937
qiibinn <17600104008@139.com>
2023-02-01 10:54:17 +0800

    Update SpringMongoCrudApplication.java

--- src/main/java/com/example/demo/SpringMongoCrudApplication.java
+++ src/main/java/com/example/demo/SpringMongoCrudApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -8,16 +7,11 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.example.demo")
-@Log4j2
 public class SpringMongoCrudApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx =
 			SpringApplication.run(SpringMongoCrudApplication.class, args);
-		
-		var mongoDbHost = ctx.getEnvironment().getProperty("spring.data.mongodb.host");
-		var mongoDbPort = ctx.getEnvironment().getProperty("spring.data.mongodb.port");
-		log.info("Connected to MongoDb: " + mongoDbHost + ":" + mongoDbPort);
 	}
 
 }
